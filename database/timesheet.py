fc6d676098694cf1314587840834fbf96c88dec6
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2020-10-25 12:36:09 +0330

    add database initillization at start

--- database/timesheet.py
+++ database/timesheet.py
@@ -3,15 +3,15 @@ from sqlalchemy.orm import relationship
 import pandas as pd
 
 
-class StockPrice(Base):
+class TimeSheet(Base):
     __tablename__ = "timesheet"
 
     id = Column(Integer, primary_key=True)
-    user = Column(id, ForeignKey("users.id"), index=True)
+    user = Column(Integer, ForeignKey("users.id"), index=True)
     time = Column(DateTime, nullable=False)
-    terminal_id = Column(String, default = '0001 : 05'
+    terminal_id = Column(String, default = '0001 : 05')
     class_ = Column('class', String, default='User')
     mode = Column(String,default="Access")
     type_ =  Column('type', String, default='User')
     result = Column(String,default="Success")
-    property = Column('type', Integer, default=1000)
+    property = Column('property', Integer, default=1000)
